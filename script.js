9f836ae8477bdf9b50ebe11e8a8b1a2792d622f6
Elliot Thiebaut <hello@elliotthiebaut.com>
2021-03-12 17:37:31 +0100

    :sparkles: Added checkout process

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ new Vue({
 		// Array qui stock les produits dans le panier de l'utilisateur
 		cart: [],
 
-		cartTotal: 0
+		// Total price of the cart
+		cartTotal: 0,
+
+		// Show success modal or not
+		returnSucess: ""
 	},
 	mounted() {
 		this.getData();
+		this.checkUrl();
 	},
 	methods: {
 		// sert à afficher la page d'un produit
@@ -31,8 +36,10 @@ new Vue({
 		// sert à ajouter un produit au panier
 		addToCart(product) {
 
+			// On vérifie si le produit est déjà dans le panier
 			let positionNewItem = this.cart.map((e) => { return e.id }).indexOf(product.id);
 			
+			// On ajoute un à la quantitté ou on crée le produit
 			if (positionNewItem >= 0) {
 				this.cart[positionNewItem].quantity ++;
 				this.cartTotal = 0
@@ -45,6 +52,7 @@ new Vue({
 					"title": product.fields.title,
 					"price": product.fields.price,
 					"image": product.fields.image[0].url,
+					"priceId": product.fields.priceID,
 					"quantity": 1
 				}
 				this.cart.push(newCartItem);
@@ -62,6 +70,8 @@ new Vue({
 		removeFromCart(product) {
 			let index = this.cart.indexOf(product);
 			this.cart.splice(index, 1);
+
+			// Mise à jour du total du panier
 			this.cartTotal = 0
 			this.cart.forEach((cartItem) => {
 				this.cartTotal += cartItem.price * cartItem.quantity
@@ -69,7 +79,26 @@ new Vue({
 		},
 
 		checkout() {
-			console.log('Paiement en cours');
+			let finalCart = [];
+			this.cart.forEach((product) => {
+				finalCart.push({
+					"price": product.priceId,
+					"quantity": product.quantity
+
+				});
+			})
+
+			stripe.redirectToCheckout({
+				// on fournit ici la liste des produits dans le panier
+				lineItems: finalCart,
+				mode: 'payment',
+				// la page de succès
+				successUrl: 'http://127.0.0.1:5500/index.html?returnCheckout=sucess',
+				// la page d'erreur
+				cancelUrl: 'http://127.0.0.1:5500/index.html?returnCheckout=fail',
+			  }).then(function (result) {
+				console.error(result.error.message);
+			  });
 		},
 
 		// sert à récupérer les données de l'API airtable
@@ -83,6 +112,25 @@ new Vue({
 
 			// On met les données dans la variable all_rows
 			this.all_products = data.records;
+		},
+
+		// On récupère les paramètres de l'URL pour afficher un modal
+		checkUrl() {
+			const queryString = window.location.search;
+			const urlParams = new URLSearchParams(queryString);
+
+			if (urlParams.has('returnCheckout')) {
+				const returnCheckout = urlParams.get('returnCheckout');
+				this.returnSucess = returnCheckout;
+			}
+		},
+
+		closeModal() {
+			modal.style.display = "none";
+		},
+
+		openModal() {
+			modal.style.display = "block";
 		}
 	}
 });
