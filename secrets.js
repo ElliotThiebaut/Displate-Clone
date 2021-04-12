98215023e068df4a8bded3a5f98955f2514c408e
Elliot Thiebaut <elliotthiebaut1@gmail.com>
2021-04-12 19:47:55 +0200

    Delete secrets.js

--- secrets.js
+++
@@ -1,5 +0,0 @@
-// Clé d'API
-const AIRTABLE_KEY = 'keyt80nD7m8zxMqDk';
-const DB_ID = 'appTTG1mVAGb6iCL2';
-const DB_TABLE = 'DISPLATE';
-const STRIPE_KEY = 'pk_test_51IUAcYHCfNmrFgUhhWD8HVXeghYQYSWd0HzrbtnZxusGqyL0EptjXELHo0BS3X2vmUmqCcq39iqPnqayG18UOddA008rtll4JZ'
